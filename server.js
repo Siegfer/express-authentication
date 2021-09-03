adf999ec75bcba2c9b3423d2b43f59299e8bfdd8
siegfer <nicksalguerobui@icloud.com>
2021-09-03 14:06:48 -0700

    add: User model and validations

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const layouts = require('express-ejs-layouts');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const layouts = require('express-ejs-layouts')
+const app = express()
 
-const SECRET_SESSION = process.env.SECRET_SESSION;
+const SECRET_SESSION = process.env.SECRET_SESSION
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.use(require('morgan')('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
-app.use(layouts);
+app.use(require('morgan')('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/public'))
+app.use(layouts)
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+	res.render('index')
+})
+
+const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
-});
+	console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`)
+})
 
-module.exports = server;
+module.exports = server
